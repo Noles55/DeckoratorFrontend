7cd90de68ea49de8e121ab736cabc59daf183d40
Nolan Byrne <nolan.byrne@comcast.net>
2021-02-14 20:15:43 -0600

    Added slider and submit button, changed styling

--- src/app/features/training/training.module.ts
+++ src/app/features/training/training.module.ts
@@ -2,11 +2,16 @@ import { SharedModule } from '../../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrainingComponent } from './training.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    MatSliderModule,
+    MatButtonModule
   ],
   declarations: [
     TrainingComponent
